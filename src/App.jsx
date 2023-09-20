77751676aa462a2c854e99c046afab4dcff7db1c
haardikshrestha <haardikshrestha@gmail.com>
2023-09-20 12:44:08 +0545

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import ExpenseItem from "./Components/ExpenseItem";
+
 function App(){
     return(
         <div>
             <h2>Lets get started!</h2>
+            <ExpenseItem></ExpenseItem>
         </div>
     )
 }
+
+export default App;
