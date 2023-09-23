a0d517643f34760ca19b08d0325c45ce9c3072b9
haardikshrestha <haardikshrestha@gmail.com>
2023-09-23 14:21:50 +0545

    minor changes

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,16 +1,16 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   const expenseDate = new Date(2023, 9, 20);
   const expenseTitle = "Car Insurance";
   const expenseAmount = 294.77;
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toString()}</div>
+      <div>{props.date.toString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
       </div>
     </div>
   );
